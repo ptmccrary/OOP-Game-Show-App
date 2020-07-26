65a8b8336479bf49057a91ac2673017118342f25
Parker McCrary <parkertomm3@gmail.com>
2020-07-26 15:52:24 -0400

    start game on btn click

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
-const game = new Game();
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+let game;
+
+document.getElementById('btn__reset').addEventListener('click', () => {
+    game = new Game();
+    game.startGame();
+});
