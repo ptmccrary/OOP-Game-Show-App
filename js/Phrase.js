86dd2bf7ab500de32e2b50ffa93911ff3f3d422b
Parker McCrary <parkertomm3@gmail.com>
2020-07-26 15:19:51 -0400

    added phrase character display

--- js/Phrase.js
+++ js/Phrase.js
@@ -3,8 +3,26 @@ class Phrase {
         this.phrase = phrase.toLowerCase(phrase);
     }
 
+    /**
+    * Display phrase on game board
+    */
     addPhraseToDisplay(){
-
+        const phraseDiv = document.getElementById('phrase');
+        const phraseUl = phraseDiv.querySelector('ul');
+        const characters = this.phrase.split('');
+        
+        for(let i = 0; i < characters.length; i++){
+            if(/[a-z]/i.test(characters[i])){
+                let letterLi = document.createElement('li');
+                letterLi.className = `hide letter ${characters[i]}`;
+                letterLi.textContent = `${characters[i]}`;
+                phraseUl.appendChild(letterLi);
+            } else{
+                let spaceLi = document.createElement('li');
+                spaceLi.className = 'space';
+                phraseUl.appendChild(spaceLi);
+            }
+        }
     }
 
     checkLetter(){
--- js/app.js
+++ js/app.js
@@ -1,11 +1,2 @@
-const logPhrase = (phrase) => {
-    console.log(`Phrase - phrase: `, phrase.phrase);
-    };
-
-    const game = new Game();
-    
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
+const game = new Game();
+game.getRandomPhrase().addPhraseToDisplay();
